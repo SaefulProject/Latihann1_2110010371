bc96674ce2185c14668f405712be47398652f5ff
SaefulProject <saefulmdrd01@gmail.com>
2023-06-12 05:39:39 -0700

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author ASUS
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika Saeful = new Matematika(7,1);
+        
+        System.out.println("Hasil Penjumlahan : "+Saeful.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+Saeful.setPengurangan());
+        System.out.println("Hasil Perkalian : "+Saeful.setPerkalian());
+        System.out.println("Hasil Pembagian : "+Saeful.setPembagian());
+    }
+ 
 }
